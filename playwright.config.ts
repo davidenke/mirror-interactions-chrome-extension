1f659322c56defb75eb6f440b9c7cfd252ee3b39
David Enke <>
2025-03-19 12:52:19 +0100

    ci: use maximum workers for tests
    and silence dev server

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,18 +14,20 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!env.CI,
   retries: env.CI ? 2 : 0,
-  workers: 4,
+  workers: env.CI ? '50%' : '75%',
   reporter: env.CI
-    ? [['html', { outputFolder: 'reports/html', open: 'never' }]]
+    ? [['list'], ['html', { outputFolder: 'reports/html', open: 'never' }]]
     : // use console, either `dot`, `line` or `list`
       [['list']],
   outputDir: 'reports/playwright',
   // defines the url for testing
   use: {
+    // we'll use a context router for `localhost/test` to proxy requests to
+    // the static `playwright.test.html`, so the host could be anything else
     baseURL: 'http://localhost',
     // create a trace if test failed
     trace: 'retain-on-failure',
   },
-  // start the dev server to build the extension
-  webServer: { command: 'npm run dev' },
+  // start the dev server of the extension silently
+  webServer: { command: 'npm run dev -- --logLevel silent' },
 });
